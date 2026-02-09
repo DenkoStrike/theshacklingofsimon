c019c966e30c0189b78a5674990c3c674638f533
William <will123iam456@gmail.com>
2026-02-08 22:43:22 -0500

    Inventory.cs implemented, partial implementation of IPlayer.cs

---
+++ TheShacklingOfSimon/Entities/IPlayer.cs
@@ -0,0 +1,20 @@
+﻿using Microsoft.Xna.Framework;
+
+namespace TheShacklingOfSimon.Entities;
+
+public interface IPlayer : IEntity, IDamageable
+{
+    Inventory Inventory { get; }
+    IWeapon CurrentWeapon { get; }
+    IItem CurrentItem { get; }
+    IPlayerState CurrentState { get; }
+    
+    void EquipItem();
+    void EquipWeapon();
+    void Attack();
+    void Move(Vector2 direction);
+
+    void ChangeState(IPlayer newState);
+    // Update() inherited from IEntity
+
+}
---
+++ TheShacklingOfSimon/Inventory.cs
@@ -0,0 +1,47 @@
+﻿using System.Collections.Generic;
+
+namespace TheShacklingOfSimon;
+
+public class Inventory
+{
+    public List<IWeapon> Weapons { get; private set; }
+    public List<IItem> Items { get; private set; }
+    
+    public Inventory()
+    {
+        Weapons = new List<IWeapon>();
+        Items = new List<IItem>();
+    }
+    
+    public void AddWeapon(IWeapon w)
+    {
+        if (!Weapons.Contains(w))
+        {
+            Weapons.Add(w);
+        }
+    }
+
+    public IWeapon RemoveWeapon(int pos)
+    {
+        if (pos < Weapons.Count)
+        {
+            Items.RemoveAt(pos);
+        }
+    }
+    
+    public void AddItem(IItem item)
+    {
+        if (!Items.Contains(item))
+        {
+            Items.Add(item);
+        }
+    }
+
+    public IItem RemoveItem(int pos)
+    {
+        if (pos < Items.Count)
+        {
+            Items.RemoveAt(pos);
+        }
+    }
+}
