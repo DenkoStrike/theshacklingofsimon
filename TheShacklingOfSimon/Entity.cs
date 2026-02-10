ba26ca8345bf73953d25d64124dbef66688e960a
DenkoStrike <107949916+DenkoStrike@users.noreply.github.com>
2026-02-09 22:31:42 -0500

    Create Entity.cs

---
+++ TheShacklingOfSimon/Entity.cs
@@ -0,0 +1,23 @@
+using System.Reflection.Metadata;
+using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework.Graphics;
+using Microsoft.Xna.Framework.Input;
+
+interface Entity
+{
+    // Common properties of *every* entity
+    public Vector2 Position { get; set; }
+    public Vector2 Velocity { get; set; }
+    public float Health { get; set; }
+    public bool IsActive { get; set; } = true;
+    public Vector2 Hitbox { get; set; }
+
+    // Abstract methods that every Entity-extending class will have to define separately
+    void Update(GameTime delta);
+    void Draw(SpriteBatch spriteBatch);
+
+    // Methods every Entity-extending class will need, but can also be overridden.
+    void TakeDamage(float amt);
+
+    void Die();
+}
